c60b9a0aa4d3fa206cf9c9a0e22609a02e9c37eb
drblockcraft <drblockcraft@gmail.com>
2017-11-23 21:38:07 -0800

    Fix

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 var Discord = require('discord.io');
 
-
 var bot = new Discord.Client({
     token: "MzgzNDM1ODc5MTY0ODA1MTIx.DPk2zg.x7PdDrq3k7VluI0TimVoD89SYgk",
     autorun: true
@@ -14,7 +13,20 @@ bot.on('message', function(user, userID, channelID, message, event) {
     if (message === "!ping") {
         bot.sendMessage({
             to: channelID,
-            message: "pong!"
+            message: "pong"
         });
     }
+    if (message === "d!lenny"){
+      bot.sendMessage({
+        to: channelID,
+        message: "( ͡° ͜ʖ ͡°)",
+      });
+    }
 });
+
+function len(){
+  var a = Math.floor((Math.random()*1)+1);
+  if(a===1){
+    console.log("( ͡° ͜ʖ ͡°)");
+  }
+}
